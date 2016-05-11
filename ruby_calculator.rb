cbfe8531781b85a7dcdc89273f70ef99eae4b1cc
Isaac Saldivar <isaacsaldivar@Isaacs-MacBook-Pro.local>
2016-05-11 14:11:06 -0600

    add comments to ruby calculator

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -1,2 +1,16 @@
+#create and edit a file
+# stage the file -git add
+# make a commit
+# push to git
 puts "Welcome to ruby calculator"
 puts "Enter your first number:"
+# get the first number
+puts "Enter your operator"
+# get your operator
+puts "Enter the second number:"
+# get the second number
+
+# figure out how to do the math on numbers
+# outpput the result to the user
+
+# Do all the bonus problems
